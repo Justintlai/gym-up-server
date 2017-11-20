411e8c851e819bc5dd068e429ec214e1bee40cb9
Justin <hello@justintlai.com>
2017-11-20 23:18:30 +0000

    route updates

--- routes/session.js
+++ routes/session.js
@@ -10,74 +10,74 @@ router.get("/", function(req, res) {
     res.send(sessions);
   });
 });
-
-//CREATE sessionMaster
-router.post("/sessionmaster/create", function(req, res) {
-  console.log("Post: Create New session!");
-  console.log("this is req.body", req.body);
-
-  models.sessionMaster
-    .create(req.body, {
-      fields: ["intensity", "start", "finish", "comments"]
-    })
-    .then(function(insertedSessionMaster) {
-      console.log(
-        "Session Master Created!" + insertedSessionMaster.get({ plain: true })
-      );
-      res.send(insertedSessionMaster.get({ plain: true }));
-    })
-    .catch(function(error) {
-      console.log(error);
-      res.send(error);
-    });
-});
-
-//CREATE sessionDetail
-router.post("/sessiondetail/create", function(req, res) {
-  console.log("Post: Create New sessionDetail!");
-  console.log("this is req.body", req.body);
-
-  models.sessionDetail
-    .create(req.body, {
-      fields: ["sessionMasterID", "workoutID", "wokroutOrder", "reps", "weight"]
-    })
-    .then(function(insertedSessionDetail) {
-      console.log(
-        "Session Detail Created!" + insertedSessionDetail.get({ plain: true })
-      );
-      res.send(insertedSessionDetail.get({ plain: true }));
-    })
-    .catch(function(error) {
-      console.log(error);
-      res.send(error);
-    });
-});
-
-// UPDATE a SessionMaster
-router.put("/sessionmaster/:sessionMasterid/update", function(req, res) {
-  console.log(req.body);
-  models.sessionMaster
-    .update(req.body, {
-      where: { sessionMasterid: req.params.sessionMasterid }
-    })
-    .then(updatedSessionMaster => {
-      console.log(updatedSessionMaster);
-      res.send(updatedSessionMaster);
-    });
-});
-
-// UPDATE a SessionDetail
-router.put("/sessiondetail/:sessionDetailid/update", function(req, res) {
-  console.log(req.body);
-  models.sessionDetail
-    .update(req.body, {
-      where: { sessionDetailid: req.params.sessionDetailid }
-    })
-    .then(updatedSessionDetail => {
-      console.log(updatedSessionDetail);
-      res.send(updatedSessionDetail);
-    });
-});
+//
+// //CREATE sessionMaster
+// router.post("/sessionmaster/create", function(req, res) {
+//   console.log("Post: Create New session!");
+//   console.log("this is req.body", req.body);
+//
+//   models.sessionMaster
+//     .create(req.body, {
+//       fields: ["intensity", "start", "finish", "comments"]
+//     })
+//     .then(function(insertedSessionMaster) {
+//       console.log(
+//         "Session Master Created!" + insertedSessionMaster.get({ plain: true })
+//       );
+//       res.send(insertedSessionMaster.get({ plain: true }));
+//     })
+//     .catch(function(error) {
+//       console.log(error);
+//       res.send(error);
+//     });
+// });
+//
+// //CREATE sessionDetail
+// router.post("/sessiondetail/create", function(req, res) {
+//   console.log("Post: Create New sessionDetail!");
+//   console.log("this is req.body", req.body);
+//
+//   models.sessionDetail
+//     .create(req.body, {
+//       fields: ["sessionMasterID", "workoutID", "wokroutOrder", "reps", "weight"]
+//     })
+//     .then(function(insertedSessionDetail) {
+//       console.log(
+//         "Session Detail Created!" + insertedSessionDetail.get({ plain: true })
+//       );
+//       res.send(insertedSessionDetail.get({ plain: true }));
+//     })
+//     .catch(function(error) {
+//       console.log(error);
+//       res.send(error);
+//     });
+// });
+//
+// // UPDATE a SessionMaster
+// router.put("/sessionmaster/:sessionMasterid/update", function(req, res) {
+//   console.log(req.body);
+//   models.sessionMaster
+//     .update(req.body, {
+//       where: { sessionMasterid: req.params.sessionMasterid }
+//     })
+//     .then(updatedSessionMaster => {
+//       console.log(updatedSessionMaster);
+//       res.send(updatedSessionMaster);
+//     });
+// });
+//
+// // UPDATE a SessionDetail
+// router.put("/sessiondetail/:sessionDetailid/update", function(req, res) {
+//   console.log(req.body);
+//   models.sessionDetail
+//     .update(req.body, {
+//       where: { sessionDetailid: req.params.sessionDetailid }
+//     })
+//     .then(updatedSessionDetail => {
+//       console.log(updatedSessionDetail);
+//       res.send(updatedSessionDetail);
+//     });
+// });
 
 // DELETE a SessionMaster
 // router.get("/:sessionMasterid/destroy", function(req, res) {
