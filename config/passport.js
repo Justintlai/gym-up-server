26628c9b90ebe667e1f17744be1b52b277a88bd0
JustinL <justinl@aerotron.co.uk>
2017-12-07 10:07:03 +0000

    server and oauth updates

--- config/passport.js
+++ config/passport.js
@@ -48,38 +48,39 @@ module.exports = function(passport) {
         // asynchronous
         process.nextTick(function() {
           // find the user in the database based on their facebook id
-          models.User.findOne({ where: { fbId: profile.id } }).then(function(user)
-          {
+          models.User.findOne({ where: { fbId: profile.id } }).then(function(
+            user
+          ) {
             // if the user is found, then log them in
             if (user) {
               return done(null, user); // user found, return that user
             } else {
               // if there is no user found with that facebook id, create them
               var data = {
-              // set all of the facebook information in our user model
-              fbId: profile.id; // set the users facebook id
-              fbToken: token; // we will save the token that facebook provides to the user
-              fbName: profile.name.givenName + " " + profile.name.familyName; // look at the passport user profile to see how names are returned
-              fbEmail: profile.emails[0].value; // facebook can return multiple emails so we'll take the first
-            };
-            //save user
-            models.User.create(data, {
-              fields: ["fbId", "fbToken", "fbName", "fbEmail"]
-            }).then(function(insertedUser) {
-              console.log(
-                "User Created!" + ": " + insertedUser.get({ plain: true })
-              );
-              // console.log("about to run DONE to go back to ROUTE");
-              // return done(null, insertedUser.get({ plain: true }));
-              if (!insertedUser) {
-                console.log("failed to insert user - nothing found!");
-                return done(null, false);
-              }
-              if (insertedUser) {
-                console.log("about to run DONE to go back to ROUTE");
-                return done(null, insertedUser);
-              }
-            });
+                // set all of the facebook information in our user model
+                fbId: profile.id, // set the users facebook id
+                fbToken: token, // we will save the token that facebook provides to the user
+                fbName: profile.name.givenName + " " + profile.name.familyName, // look at the passport user profile to see how names are returned
+                fbEmail: profile.emails[0].value // facebook can return multiple emails so we'll take the first
+              };
+              //save user
+              models.User.create(data, {
+                fields: ["fbId", "fbToken", "fbName", "fbEmail"]
+              }).then(function(insertedUser) {
+                console.log(
+                  "User Created!" + ": " + insertedUser.get({ plain: true })
+                );
+                // console.log("about to run DONE to go back to ROUTE");
+                // return done(null, insertedUser.get({ plain: true }));
+                if (!insertedUser) {
+                  console.log("failed to insert user - nothing found!");
+                  return done(null, false);
+                }
+                if (insertedUser) {
+                  console.log("about to run DONE to go back to ROUTE");
+                  return done(null, insertedUser);
+                }
+              });
             }
           });
         });
@@ -143,4 +144,5 @@ module.exports = function(passport) {
       }
     )
   );
+};
 };
