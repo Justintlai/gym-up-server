e81355c8315e2719429bde146a4c7f8a069b2208
Justin <hello@justintlai.com>
2017-12-07 00:27:22 +0000

    add passport and google auth

--- config/passport.js
+++ config/passport.js
@@ -118,17 +118,18 @@ module.exports = function(passport) {
               models.User.create(data, {
                 fields: ["ggId", "ggToken", "ggName", "ggEmail"]
               }).then(function(insertedUser) {
+                return done(null, insertedUser);
                 console.log(
                   "User Created!" + ": " + insertedUser.get({ plain: true })
                 );
-                if (!insertedUser) {
-                  console.log("failed to insert user - nothing found!");
-                  return done(null, false);
-                }
-                if (insertedUser) {
-                  console.log("about to run DONE to go back to ROUTE");
-                  return done(null, insertedUser);
-                }
+                // if (!insertedUser) {
+                //   console.log("failed to insert user - nothing found!");
+                //   return done(null, false);
+                // }
+                // if (insertedUser) {
+                //   console.log("about to run DONE to go back to ROUTE");
+                //   return done(null, insertedUser);
+                // }
               });
             }
           });
