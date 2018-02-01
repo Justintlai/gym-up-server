24998454f18158ea38efcd5507b70cd8212c64a9
Justin <hello@justintlai.com>
2018-02-01 13:08:01 +0000

    get seesion

--- modules/data-manager.js
+++ modules/data-manager.js
@@ -4,97 +4,138 @@ var models = require("../models");
  * gets the list of sessions associated with the user
  * */
 exports.getAllSessions = function(userId, callback) {
-  models.sessionMaster
-    .findAll({ include: [{ model: models.sessionDetail }, { model: models.User }], where: { userId: userId } }, { raw: true })
-    .then(function(sessions) {
-      callback(sessions);
-    });
+    models.sessionMaster
+        .findAll(
+            {
+                include: [
+                    {
+                        model: models.sessionDetail
+                    },
+                    {
+                        model: models.User
+                    }
+                ],
+                where: {
+                    userId: userId
+                }
+            },
+            {
+                raw: true
+            }
+        )
+        .then(function(sessions) {
+            callback(sessions);
+        });
+};
+
+exports.getSession = function(userId, sessionId, callback) {
+    models.sessionMaster
+        .findAll(
+            {
+                include: [
+                    { model: models.sessionDetail },
+                    { model: models.User }
+                ],
+                where: {
+                    $and: [
+                        { "$sessionMaster.Id$": sessionId },
+                        { "$sessionMaster.userId$": userId }
+                    ]
+                }
+            },
+            { raw: true }
+        )
+        .then(session => {
+            callback(session);
+        });
 };
 
 /**
  * creates a new car and puts it in the database
  * */
 exports.createCar = function(userId, newData, callback) {
-  var car = { ownerId: userId };
-  if (newData.make) car.make = newData.make;
-  if (newData.model) car.model = newData.model;
-  if (newData.color) car.color = newData.color;
-  if (newData.licensePlate) car.licensePlate = newData.licensePlate;
+    var car = {
+        ownerId: userId
+    };
+    if (newData.make) car.make = newData.make;
+    if (newData.model) car.model = newData.model;
+    if (newData.color) car.color = newData.color;
+    if (newData.licensePlate) car.licensePlate = newData.licensePlate;
 
-  models.Car.create(car).then(function(car) {
-    callback(car);
-  });
+    models.Car.create(car).then(function(car) {
+        callback(car);
+    });
 };
 
 /**
  * gets a car by id
  * */
 exports.getCar = function(carId, callback) {
-  models.Car.find({
-    where: {
-      id: carId
-    }
-  }).then(function(car) {
-    callback(car);
-  });
+    models.Car.find({
+        where: {
+            id: carId
+        }
+    }).then(function(car) {
+        callback(car);
+    });
 };
 
 /**
  * updates the car info by id
  * */
 exports.updateCarInfo = function(userId, newData, callback) {
-  models.Car.find({
-    where: {
-      id: newData.id
-    }
-  }).then(function(car) {
-    if (car) {
-      if (car.ownerId != userId) {
-        return callback(null, "You don't own that car");
-      }
-      if (newData.make) car.make = newData.make;
-      if (newData.model) car.model = newData.model;
-      if (newData.color) car.color = newData.color;
-      if (newData.licensePlate) car.licensePlate = newData.licensePlate;
-      car.save().then(saved => {
-        callback(saved);
-      });
-    } else {
-      callback(null, "Car not found");
-    }
-  });
+    models.Car.find({
+        where: {
+            id: newData.id
+        }
+    }).then(function(car) {
+        if (car) {
+            if (car.ownerId != userId) {
+                return callback(null, "You don't own that car");
+            }
+            if (newData.make) car.make = newData.make;
+            if (newData.model) car.model = newData.model;
+            if (newData.color) car.color = newData.color;
+            if (newData.licensePlate) car.licensePlate = newData.licensePlate;
+            car.save().then(saved => {
+                callback(saved);
+            });
+        } else {
+            callback(null, "Car not found");
+        }
+    });
 };
 
 /**
  * deletes a car object
  * */
 exports.deleteCar = function(userId, carId, callback) {
-  models.Car.find({
-    where: {
-      id: carId
-    }
-  }).then(function(car) {
-    if (car) {
-      //check if the user is the owner of the car otherwise they can't delete it
-      if (car.ownerId != userId) {
-        return callback(null, "You don't own that car");
-      }
-      car.destroy().then(callback);
-    } else {
-      callback(null, "Car not found");
-    }
-  });
+    models.Car.find({
+        where: {
+            id: carId
+        }
+    }).then(function(car) {
+        if (car) {
+            //check if the user is the owner of the car otherwise they can't delete it
+            if (car.ownerId != userId) {
+                return callback(null, "You don't own that car");
+            }
+            car.destroy().then(callback);
+        } else {
+            callback(null, "Car not found");
+        }
+    });
 };
 
 /**
  * gets the list of all users
  * */
 exports.getAllUsers = function(callback) {
-  models.User.findAll().then(function(users) {
-    if (users) {
-      callback(users);
-    } else {
-      callback(null);
-    }
-  });
+    models.User.findAll().then(function(users) {
+        if (users) {
+            callback(users);
+        } else {
+            callback(null);
+        }
+    });
 };
--- routes/session.js
+++ routes/session.js
@@ -16,124 +16,150 @@ var DM = require("../modules/data-manager");
  * ========================================================
  * */
 router.get("/", function(req, res) {
-  console.log("Request: Get All SESSIONS for a user! ");
-  var user = req.user;
-  console.log("user id: ", user.id);
+    console.log("Request: Get All SESSIONS for a user! ");
+    var user = req.user;
+    console.log("user id: ", user.id);
 
-  DM.getAllSessions(user.id, function(sessions) {
-    res.status(200).send({ status: 200, sessions: sessions });
-  });
+    DM.getAllSessions(user.id, function(sessions) {
+        res.status(200).send({ status: 200, sessions: sessions });
+    });
+});
+
+/**
+ * ========================================================
+ * GET specific session
+ * ========================================================
+ * */
+router.get("/:sessionId", function(req, res) {
+    console.log("Request:Get session! ");
+    var sessionId = req.params.sessionId;
+    var user = req.user;
+    console.log("user id: ", user.id);
+    console.log("session id: ", req.params.sessionId);
+
+    DM.getSession(user.id, sessionId, function(session) {
+        console.log(session);
+        res.status(200).send({ status: 200, session: session });
+    });
 });
 
 //CREATE sessionMaster
 router.post("/sessionmaster", function(req, res) {
-  console.log("Post: Create New session!");
-  console.log("this is req.body", req.body);
+    console.log("Post: Create New session!");
+    console.log("this is req.body", req.body);
 
-  models.sessionMaster
-    .create(req.body, {
-      fields: [
-        "userId",
-        "sessionName",
-        "intensity",
-        "start",
-        "finish",
-        "comments"
-      ]
-    })
-    .then(function(insertedSessionMaster) {
-      console.log(
-        "Session Master Created!" + insertedSessionMaster.get({ plain: true })
-      );
-      res.send(insertedSessionMaster.get({ plain: true }));
-    })
-    .catch(function(error) {
-      console.log(error);
-      res.send(error);
-    });
+    models.sessionMaster
+        .create(req.body, {
+            fields: [
+                "userId",
+                "sessionName",
+                "intensity",
+                "start",
+                "finish",
+                "comments"
+            ]
+        })
+        .then(function(insertedSessionMaster) {
+            console.log(
+                "Session Master Created!" +
+                    insertedSessionMaster.get({ plain: true })
+            );
+            res.send(insertedSessionMaster.get({ plain: true }));
+        })
+        .catch(function(error) {
+            console.log(error);
+            res.send(error);
+        });
 });
 
 //CREATE sessionDetail
 router.post("/sessiondetail", function(req, res) {
-  console.log("Post: Create New sessionDetail!");
-  console.log("this is req.body", req.body);
+    console.log("Post: Create New sessionDetail!");
+    console.log("this is req.body", req.body);
 
-  models.sessionDetail
-    .create(req.body, {
-      fields: ["sessionMasterId", "workoutId", "workoutOrder", "reps", "weight"]
-    })
-    .then(function(insertedSessionDetail) {
-      console.log(
-        "Session Detail Created!" + insertedSessionDetail.get({ plain: true })
-      );
-      res.send(insertedSessionDetail.get({ plain: true }));
-    })
-    .catch(function(error) {
-      console.log(error);
-      res.send(error);
-    });
+    models.sessionDetail
+        .create(req.body, {
+            fields: [
+                "sessionMasterId",
+                "workoutId",
+                "workoutOrder",
+                "reps",
+                "weight"
+            ]
+        })
+        .then(function(insertedSessionDetail) {
+            console.log(
+                "Session Detail Created!" +
+                    insertedSessionDetail.get({ plain: true })
+            );
+            res.send(insertedSessionDetail.get({ plain: true }));
+        })
+        .catch(function(error) {
+            console.log(error);
+            res.send(error);
+        });
 });
 
 // UPDATE a SessionMaster
 router.put("/sessionmaster/:sessionMasterid", function(req, res) {
-  console.log(req.body);
-  models.sessionMaster
-    .update(req.body, {
-      where: { sessionMasterid: req.params.sessionMasterid }
-    })
-    .then(updatedSessionMaster => {
-      console.log(updatedSessionMaster);
-      res.send(updatedSessionMaster);
-    });
+    console.log(req.body);
+    models.sessionMaster
+        .update(req.body, {
+            where: { sessionMasterid: req.params.sessionMasterid }
+        })
+        .then(updatedSessionMaster => {
+            console.log(updatedSessionMaster);
+            res.send(updatedSessionMaster);
+        });
 });
 
 // UPDATE a SessionDetail
 router.put("/sessiondetail/:sessionDetailid", function(req, res) {
-  console.log(req.body);
-  models.sessionDetail
-    .update(req.body, {
-      where: { sessionDetailid: req.params.sessionDetailid }
-    })
-    .then(updatedSessionDetail => {
-      console.log(updatedSessionDetail);
-      res.send(updatedSessionDetail);
-    });
+    console.log(req.body);
+    models.sessionDetail
+        .update(req.body, {
+            where: { sessionDetailid: req.params.sessionDetailid }
+        })
+        .then(updatedSessionDetail => {
+            console.log(updatedSessionDetail);
+            res.send(updatedSessionDetail);
+        });
 });
 
 //DELETE a SessionMaster
 router.delete("/sessionmaster/:sessionMasterid", function(req, res) {
-  const masterId = req.params.sessionMasterid;
-  models.sessionDetail
-    .destroy({
-      where: {
-        sessionMasterid: masterId
-      }
-    })
-    .then(() => {
-      //Destroy SessionMaster WHERE SessionMasterID
-      models.sessionMaster
+    const masterId = req.params.sessionMasterid;
+    models.sessionDetail
         .destroy({
-          where: {
-            sessionMasterid: masterId
-          }
+            where: {
+                sessionMasterid: masterId
+            }
         })
         .then(() => {
-          res.send("session master deleted!");
+            //Destroy SessionMaster WHERE SessionMasterID
+            models.sessionMaster
+                .destroy({
+                    where: {
+                        sessionMasterid: masterId
+                    }
+                })
+                .then(() => {
+                    res.send("session master deleted!");
+                });
         });
-    });
 });
 
 //DELETE a SessionMaster
 router.delete("/sessiondetail/:sessiondetailid", function(req, res) {
-  models.sessionDetail
-    .destroy({
-      where: {
-        sessiondetailid: req.params.sessiondetailid
-      }
-    })
-    .then(() => {
-      res.send("SessionDetail Deleted");
-    });
+    models.sessionDetail
+        .destroy({
+            where: {
+                sessiondetailid: req.params.sessiondetailid
+            }
+        })
+        .then(() => {
+            res.send("SessionDetail Deleted");
+        });
 });
 
 module.exports = router;
