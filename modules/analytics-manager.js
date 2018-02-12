e9efd1b9539d38d114f7df448023d8a39a3a8b0a
Justin <hello@justintlai.com>
2018-02-12 11:41:15 +0000

    update analytics route

--- modules/analytics-manager.js
+++ modules/analytics-manager.js
@@ -64,7 +64,7 @@ exports.workoutData = function(userId, workoutId, callback) {
 exports.workoutStats = function(callback) {
     models.sequelize
         .query(
-            "SELECT SM.`sessionMasterId`,(SD.`weight` * SD.`reps`) AS TotalWeight FROM `sessionMasters` AS SM INNER JOIN `sessionDetails` SD ON SM.`sessionMasterId` = SD.`sessionDetailId`",
+            "SELECT SM.`id`,(SD.`weight` * SD.`reps`) AS TotalWeight FROM `sessionMasters` AS SM INNER JOIN `sessionDetails` SD ON SM.`id` = SD.`sessionMasterId`",
             {
                 type: models.sequelize.QueryTypes.SELECT
             }
@@ -77,7 +77,7 @@ exports.workoutStats = function(callback) {
 exports.workoutMuscleGroup = function(callback) {
     models.sequelize
         .query(
-            "SELECT COUNT(`bodyPart`) AS Count, `bodyPart` FROM `sessionMasters` AS SM INNER JOIN `sessionDetails` SD ON SM.`sessionMasterId` = SD.`sessionDetailId` INNER JOIN `Workouts` W ON W.`workoutId` = SD.`WorkoutId` GROUP BY W.`bodyPart`",
+            "SELECT COUNT(`bodyPart`) AS Count, `bodyPart` FROM `sessionMasters` AS SM INNER JOIN `sessionDetails` SD ON SM.`id` = SD.`sessionMasterId` INNER JOIN `Workouts` W ON W.`workoutId` = SD.`WorkoutId` GROUP BY W.`bodyPart`",
             {
                 type: models.sequelize.QueryTypes.SELECT
             }
