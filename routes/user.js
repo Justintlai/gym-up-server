1a8c4958902c118338aa05c4c59e65f3c672f318
Justin <hello@justintlai.com>
2018-02-12 10:57:26 +0000

    updated workout route

--- routes/user.js
+++ routes/user.js
@@ -4,76 +4,73 @@ var router = express.Router();
 
 //default route to get users
 router.get("/", function(req, res) {
-  console.log("Request: Get User Profile!");
-  var user = req.user;
+    console.log("Request: Get User Profile!");
+    var user = req.user;
 
-  DM.getUser(user.id, function(user){
-    res
-      .status(200)
-      .send({
-        status: 200,
-        message: "User Profile!",
-        user: user
-      });
-  });
+    if (!user) {
+        return res.status(400).send({
+            status: 400,
+            message: "No session id specified"
+        });
+    }
+
+    DM.getUser(user.id, function(user) {
+        res.status(200).send({
+            status: 200,
+            message: "User Profile!",
+            user: user
+        });
+    });
 });
 
 // UPDATE a user
-router.put("/:userid", function(req, res) {
-  console.log("Request: UDPATE User Profile!");
-  var user = req.user;
+router.put("/", function(req, res) {
+    console.log("Request: UPDATE User Profile!");
+    var user = req.user;
 
-  if (!user) {
-    return res
-      .status(400)
-      .send({
-        status: 400,
-        message: "No session id specified"
-      });
-  }
+    if (!user) {
+        return res.status(400).send({
+            status: 400,
+            message: "No session id specified"
+        });
+    }
 
-  var post = req.body; 
-  var newData = {};
-  if (post.firstName) newData.firstName = post.firstName;
-  if (post.lastName) newData.lastName = post.lastName;
-  if (post.email) newData.email = post.email;
-  if (post.password) newData.password = post.password;
+    var post = req.body;
+    var newData = {};
+    if (post.firstName) newData.firstName = post.firstName;
+    if (post.lastName) newData.lastName = post.lastName;
+    if (post.email) newData.email = post.email;
+    if (post.password) newData.password = post.password;
 
-  DM.updateUser(user.id, newData, function(user){
-    res
-      .status(200)
-      .send({
-        status: 200,
-        message: "User Profile Updated!",
-        user: user
-      });
-  });
+    DM.updateUser(user.id, newData, function(user) {
+        res.status(200).send({
+            status: 200,
+            message: "User Profile Updated!",
+            user: user
+        });
+    });
 });
 
 // DELETE a user
-router.delete("/:userid", function(req, res) {
-  console.log("Request: DELETE User Profile!");
-  var user = req.user;
-   if (!user) {
-     return res
-       .status(400)
-       .send({
-         status: 400,
-         message: "No session id specified"
-       });
-   }
-   DM.deleteUser(user.id, function(deletedUser){
-    if (deletedUser) {
-      res.status(200).send({ status: 200, message: "Deleted User" });
-    } else {
-      res
-        .status(400)
-        .send({
-          status: 400,
-          message: "Can't delete User"
+router.delete("/", function(req, res) {
+    console.log("Request: DELETE User Profile!");
+    var user = req.user;
+    if (!user) {
+        return res.status(400).send({
+            status: 400,
+            message: "No session id specified"
         });
     }
-   });
+    DM.deleteUser(user.id, function(deletedUser) {
+        if (deletedUser) {
+            res.status(200).send({ status: 200, message: "Deleted User" });
+        } else {
+            res.status(400).send({
+                status: 400,
+                message: "Can't delete User"
+            });
+        }
+    });
 });
 
 module.exports = router;
