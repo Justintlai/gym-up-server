dd57e7348f8254e43e5701f4ac8c759898f97d2c
Justin <hello@justintlai.com>
2018-02-12 10:51:02 +0000

    updated workout route

--- routes/workout.js
+++ routes/workout.js
@@ -8,17 +8,21 @@ var DM = require("../modules/data-manager");
  * ========================================================
  * */
 router.get("/", function(req, res) {
-  console.log("Request: Get All Workouts! ");
+    console.log("Request: Get All Workouts! ");
 
-  DM.getAllWorkouts(function(workouts, err) {
-    if (workouts) {
-      res
-        .status(200)
-        .send({ status: 200, message: "All Workouts!", workouts: workouts });
-    } else {
-      res.status(400).send({ status: 400, message: err });
-    }
-  });
+    DM.getAllWorkouts(function(workouts, err) {
+        if (workouts) {
+            res
+                .status(200)
+                .send({
+                    status: 200,
+                    message: "All Workouts!",
+                    workouts: workouts
+                });
+        } else {
+            res.status(400).send({ status: 400, message: err });
+        }
+    });
 });
 
 /**
@@ -27,59 +31,56 @@ router.get("/", function(req, res) {
  * ========================================================
  * */
 router.post("/", (req, res) => {
-  console.log("POST: CREATE NEW WORKOUT!");
-  
-  var post = req.body;
-  newData = {};
-  if (post.name) newData.name = post.name;
-  if (post.bodyPart) newData.bodyPart = post.bodyPart;
-  if (post.description) newData.description = post.description;
-  if (post.videoURL) newData.videoURL = post.videoURL;
+    console.log("POST: CREATE NEW WORKOUT!");
 
-  DM.createWorkout(newData, function(workout){
-    res.status(200).send({
-      status:200,
-      message:"Workout Created",
-      workout: workout
-    }); 
-  });
+    var post = req.body;
+    newData = {};
+    if (post.name) newData.name = post.name;
+    if (post.bodyPart) newData.bodyPart = post.bodyPart;
+    if (post.description) newData.description = post.description;
+    if (post.videoURL) newData.videoURL = post.videoURL;
+
+    DM.createWorkout(newData, function(workout) {
+        res.status(200).send({
+            status: 200,
+            message: "Workout Created",
+            workout: workout
+        });
+    });
 });
 /**
  * ========================================================
  * UPDATE a workout
  * ========================================================
  * */
-router.put("/:workoutId", (req,res)=>{
-  console.log("PUT: UPDATE WORKOUT");
-  var workoutId = req.params.workoutId;
-  if(!workoutId){
-      return res
-        .status(400)
-        .send({
-          status: 400,
-          message: "No session id specified"
+router.put("/:workoutId", (req, res) => {
+    console.log("PUT: UPDATE WORKOUT");
+    var workoutId = req.params.workoutId;
+    if (!workoutId) {
+        return res.status(400).send({
+            status: 400,
+            message: "No session id specified"
         });
-  }
-  var post = req.body;
-  newData = {};
-  if (post.name) newData.name = post.name;
-  if (post.bodyPart) newData.bodyPart = post.bodyPart;
-  if (post.description) newData.description = post.description;
-  if (post.videoURL) newData.videoURL = post.videoURL;
-  DM.updateWorkout(workoutId, newData, function(workout, err){
-    if(workout){
-      res.status(200).send({
-        status:200,
-        message:"Workout Updated!",
-        workout: workout
-      }); 
-    } else {
-      console.log(err);
-      res.status(400).send({ status: 400, message: err });
     }
-  });
+    var post = req.body;
+    newData = {};
+    if (post.name) newData.name = post.name;
+    if (post.bodyPart) newData.bodyPart = post.bodyPart;
+    if (post.description) newData.description = post.description;
+    if (post.videoURL) newData.videoURL = post.videoURL;
+    DM.updateWorkout(workoutId, newData, function(workout, err) {
+        if (workout) {
+            res.status(200).send({
+                status: 200,
+                message: "Workout Updated!",
+                workout: workout
+            });
+        } else {
+            console.log(err);
+            res.status(400).send({ status: 400, message: err });
+        }
+    });
 });
-
 
 /**
  * ========================================================
@@ -87,28 +88,24 @@ router.put("/:workoutId", (req,res)=>{
  * ========================================================
  * */
 router.delete("/:workoutId", function(req, res) {
-  console.log("DELETE: DESTROY WORKOUT");  
-  var workoutId = req.params.workoutID;
-  if(!workoutId){
-    return res
-      .status(400)
-      .send({
-        status: 400,
-        message: "No Workout id specified"
-      });
-  }
-
-  DM.deleteWorkout(workoutId, function(deletedWorkout){
-    if (deletedSession) {
-      res.status(200).send({ status: 200, message: "Deleted Workout" });
-    } else {
-      res
-        .status(400)
-        .send({
-          status: 400,
-          message: "Can't delete Workout"
+    console.log("DELETE: DESTROY WORKOUT");
+    var workoutId = req.params.workoutId;
+    if (!workoutId) {
+        return res.status(400).send({
+            status: 400,
+            message: "No Workout id specified"
         });
     }
-  });
+
+    DM.deleteWorkout(workoutId, function(deletedWorkout) {
+        if (deletedSession) {
+            res.status(200).send({ status: 200, message: "Deleted Workout" });
+        } else {
+            res.status(400).send({
+                status: 400,
+                message: "Can't delete Workout"
+            });
+        }
+    });
 });
 module.exports = router;
