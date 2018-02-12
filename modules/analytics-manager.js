605afc1221485622d85a3d02b493e3d0c2a2f7fb
Justin <hello@justintlai.com>
2018-02-12 11:18:30 +0000

    update analytics route

---
+++ modules/analytics-manager.js
@@ -0,0 +1,23 @@
+var models = require("../models");
+
+module.sessionData = function(userId, callback) {
+    models.sessionMaster
+        .findAll(
+            {
+                attributes: ["start", "intensity"],
+                include: [
+                    {
+                        model: models.sessionDetail,
+                        attributes: [],
+                        required: true
+                    },
+                    { model: models.User, attributes: [], required: true }
+                ],
+                where: { "$sessionMaster.userId$": req.body.userId }
+            },
+            { raw: true }
+        )
+        .then(sessions => {
+            res.send(sessions);
+        });
+};
